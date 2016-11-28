15d653eecf700f878dff213a547d58d47212076e
Keyen331 <banayos.kay@gmail.com>
2016-11-28 08:43:19 -1000

    uploading

---
+++ loops.js
@@ -0,0 +1,21 @@
+//Loops City
+
+//I can count 0-100
+
+var count;
+for (count = 0; count <= 100; count++) {
+  console.log("I can Count " + count);
+}
+
+//Noah Arc
+var animalCount;
+for (animalCount = 5; animalCount <10; animalCount++){
+  console.log("animal number " + animalCount);
+  console.log("there are " + animalCount + "many animal in Noah Arc");
+}
+
+//Hip Hop Array
+var partyAnimals = [1, 2, 3, 4, 5];
+for (partyAnimals = ; partyAnimals >= 0; partyAnimals++) {
+  
+}

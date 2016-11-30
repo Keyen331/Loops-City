d39091aa1e83c84d29d23b2d1f256898fcde4c3b
Keyen331 <banayos.kay@gmail.com>
2016-11-30 09:19:08 -1000

    updating

--- loops.js
+++ loops.js
@@ -11,11 +11,24 @@ for (count = 0; count <= 100; count++) {
 var animalCount;
 for (animalCount = 5; animalCount <10; animalCount++){
   console.log("animal number " + animalCount);
-  console.log("there are " + animalCount + "many animal in Noah Arc");
+  console.log("there are " + animalCount + " many animal in Noah Arc");
 }
 
 //Hip Hop Array
-var partyAnimals = [1, 2, 3, 4, 5];
-for (partyAnimals = ; partyAnimals >= 0; partyAnimals++) {
-  
+
+
+//Merlin's Battle Weapon
+var weaponChest = ['Mace', 'Sorcerer Stone', 'Excelibur'];
+var hero = {
+  wizard : 'Merlin',
+  weapon : weaponChest[2],
+  power : 30
+}
+function MerlinWeapon(x){
+  if (hero.weapon == weaponChest[1]) {
+    console.log(hero.wizard + " shall slay the dragon with the " + hero.weapon);
+  }else{
+    hero.weapon = weaponChest[1];
+    console.log(hero.wizard + " is ready for war with the " + hero.weapon);
+  }
 }
